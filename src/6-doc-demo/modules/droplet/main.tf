4e8b9ccc0dd32cdddbe632af8f2decdbe2f13d5d
Daniel Moraes <moraesdam@gmail.com>
2020-09-21 22:20:51 -0700

    adding firewall rules using dynamic blocks

--- src/6-doc-demo/modules/droplet/main.tf
+++ src/6-doc-demo/modules/droplet/main.tf
@@ -5,3 +5,29 @@ resource "digitalocean_droplet" "droplet" {
   region = var.region
   size   = var.size
 }
+
+resource "digitalocean_firewall" "web" {
+  name = "web-firewall-rules"
+
+  droplet_ids = [digitalocean_droplet.droplet.id]
+
+  dynamic "inbound_rule" {
+    for_each = var.default_firewall
+    content {  
+      protocol         = inbound_rule.value["protocol"]
+      port_range       = inbound_rule.key
+      source_addresses = inbound_rule.value["source_addresses"]
+    }
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
--- src/6-doc-demo/modules/droplet/variables.tf
+++ src/6-doc-demo/modules/droplet/variables.tf
@@ -11,3 +11,12 @@ variable "image" {
 }
 
 variable "size" {}
+
+variable "default_firewall" {
+  type = map(object({description = string, protocol = string, source_addresses = list(string)}))
+  default = {
+    22 = { description = "ssh", protocol = "tcp", source_addresses = ["192.168.1.0/24", "2002:1:2::/48"] }
+    80 = { description = "http", protocol = "tcp", source_addresses = ["192.168.1.0/24", "2002:1:2::/48"] }
+    "icmp" = { description = "icmp", protocol = "icmp", source_addresses = ["0.0.0.0/0", "::/0"] }
+  }
+}
